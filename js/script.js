907ec4624cb23a45703e79c3a49620ada93e99c7
Ana Luiza Gomes Porto <analuiza.gp.veleiros@gmail.com>
2024-09-19 16:41:34 -0300

    update js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ const toggleTheme = document.getElementById("toggleTheme");
 
 const rootHtml = document.documentElement;
 
+const accordionHeaders = document. querySlectorAll;
+
 
 //função para alternar o tema
 function changeTheme ()  {
-    const currentTheme = rootHtml.getAttribute("data-theme");
+    const currentTheme = rootHtml.getAttribute("data-theme");("accordion-header");
+    
 
     currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") 
     :rootHtml.setAttribute("data-theme", "dark");
@@ -17,3 +20,16 @@ function changeTheme ()  {
 
 toggleTheme.addEventListener("click", changeTheme);
 
+toggleTheme.addEventListener("click", changeTheme);
+/* Fim função mudar de tema*/
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () =>{
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains
+        ("active");
+
+        accordionActive ? accordionItem.classList.remove("active") :
+        accordionItem.classList.add("active");
+    })
+} )
